afb38174e6fbfab6cccd6ed4b30679cd91f85a32
Severino Neto <severinoneto591@gmail.com>
2026-09-14 10:01:41 -0300

    Add files via upload

---
+++ P2/Desenvolvimento de Aplicações para Internet/Aula0914/cadastro.js
@@ -0,0 +1,12 @@
+const nome = "Neto";
+const idade = 18;
+const curso = "ADS";
+const cidade = "Orobó";
+
+console.log("Nome: " + nome)
+console.log("Idade: " + idade)
+console.log("Curso: " + curso)
+console.log("Cidade: " + cidade)
+
+const idadeProxAno = idade + 1;
+console.log("Idade no proximo ano: " + idadeProxAno);
---
+++ P2/Desenvolvimento de Aplicações para Internet/Aula0914/media.js
@@ -0,0 +1,14 @@
+const nota1 = 7.0;
+const nota2 = 6.0;
+const nota3 = 5.0;
+const nota4 = 5.0;
+
+const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log(media)
+
+if (media >= 7) {
+    console.log("Aprovado")
+} else {
+  console.log("Reprovado")  
+}
