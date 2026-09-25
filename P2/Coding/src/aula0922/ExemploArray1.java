042ee2c0b453a97d1864e7a385018d2054befc27
Severino Neto <severinoneto591@gmail.com>
2026-09-25 20:44:38 -0300

    Rename variable for array syntax clarity

--- P2/Coding/src/aula0922/ExemploArray1.java
+++ P2/Coding/src/aula0922/ExemploArray1.java
@@ -6,7 +6,7 @@ public class ExemploArray1 {
 		
 		// Instanciação
 		String [] listaDeNomes; // sintaxe atual (recomendada)
-		String listaDeNomes []; // sintaxe antiga
+		String listaDeNomesAntigo []; // sintaxe antiga
 		
 		// Inicialização
 		listaDeNomes = new String [10];
